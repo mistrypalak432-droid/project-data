c01caf6220ca3d49e3ab4242e10fb2aa2d89e5fa
mistrypalak432-droid <mistrypalak432@gmail.com>
2026-05-03 21:10:31 +0530

    update

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Running on " + PORT);
+});
